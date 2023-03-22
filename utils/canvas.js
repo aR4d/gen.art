82c14433780ff5170b9c5844b7171ae1a118c618
AlexandruRad <alexandru.rad@icloud.com>
2023-03-22 23:42:55 +0200

    still cannot control download pixel res

--- utils/canvas.js
+++ utils/canvas.js
@@ -1,71 +1,52 @@
-class Canvas {
-  // The artwork is driven by Height or Width but not both.
-  constructor(desired_height) {
-    this.DEFAULT_HEIGHT = 400;
-    this.ASPECT_RATIO = 3 / 4;
-    this.cvs = D.body.appendChild(D.createElement("canvas"));
-    this.cvs.height = max(desired_height, this.DEFAULT_HEIGHT);
-    this.cvs.width = ~~(this.cvs.height * this.ASPECT_RATIO);
-  }
+// Creates an HTML canvas element that:
+// 1.Scales on high density retina display resolutions
+// 2.Scales for quality print of printed artwork at specific pixel size
+const Canvas = function (targetHeight, backgroundHexColor) {
+  const REF_HEIGHT = 400;
+  const ASPECT_RATIO = 3 / 4;
 
-  background(hexColor) {
-    this.ctx.fillStyle = hexColor;
-    this.ctx.fillRect(0, 0, this.cvs.width, this.cvs.height);
-  }
+  this.init = function () {
+    const h = targetHeight;
+    const w = ~~(targetHeight * ASPECT_RATIO);
 
-  set_style_on_resize() {
-    W.onresize = this.set_style;
-    this.set_style();
-  }
+    // Create the canvas element
+    const cvs = D.body.appendChild(D.createElement("canvas"));
 
-  set_style() {
-    const s = this.cvs.style;
-    s.position = "absolute";
-    s.display = "block";
-    s.top = s.left = s.right = s.bottom = "0";
-    s.margin = "auto";
-    if (W.innerWidth / W.innerHeight < this.ASPECT_RATIO) {
-      s.width = "100%";
-      s.height = "auto";
-    } else {
-      s.width = "auto";
-      s.height = "100%";
-    }
-  }
-
-  set_pixel_density() {
     // Get the DPR and size of the canvas
     const dpr = W.devicePixelRatio;
-    const rect = this.cvs.getBoundingClientRect();
 
     // Set the "actual" size of the canvas
-    this.cvs.width = rect.width * dpr;
-    this.cvs.height = rect.height * dpr;
+    cvs.height = ~~(h * dpr);
+    cvs.width = ~~(w * dpr);
 
     // Set the "drawn" size of the canvas
-    this.cvs.style.width = `${rect.width}px`;
-    this.cvs.style.height = `${rect.height}px`;
+    cvs.style.height = `${h}px`;
+    cvs.style.width = `${w}px`;
 
     // Fetch the context.
-    const ctx = this.cvs.getContext("2d", { alpha: false });
+    const ctx = cvs.getContext("2d", { alpha: false });
 
     // Scale the context to ensure correct drawing operations
     ctx.scale(dpr, dpr);
 
-    // TODO: remove this
-    this.print_stats();
+    // Set background color
+    ctx.fillStyle = backgroundHexColor;
+    ctx.fillRect(0, 0, cvs.width, cvs.height);
 
-    // Return the modified context.
+    // Log canvas dimensions
+    log(cvs);
+
     return ctx;
-  }
+  };
 
-  print_stats() {
-    const pixelRatio = W.devicePixelRatio;
-    const sizeOnScreen = this.cvs.getBoundingClientRect();
+  // Do not include this in production code
+  function log(canvas) {
+    const sizeOnScreen = canvas.getBoundingClientRect();
 
-    console.log(`Device Pixel Ratio: ${pixelRatio}`);
+    console.log(`Aspect Ratio: ${ASPECT_RATIO}`);
+    console.log(`Device Pixel Ratio: ${W.devicePixelRatio}`);
     console.log(`Canvas size on screen: W = ${sizeOnScreen.width} H = ${sizeOnScreen.height}`);
-    console.log(`Canvas HTML: W = ${this.cvs.width} H = ${this.cvs.height}`);
-    console.log(`Canvas CSS: W = ${this.cvs.style.width} H = ${this.cvs.style.height}`);
+    console.log(`Canvas HTML: W = ${canvas.width} H = ${canvas.height}`);
+    console.log(`Canvas CSS: W = ${canvas.style.width} H = ${canvas.style.height}`);
   }
-}
+};
--- utils/globals.js
+++ utils/globals.js
@@ -6,7 +6,7 @@ const round = Math.round,
   max = Math.max,
   rand = Math.random;
 
-let tokenData = {
+const tokenData = {
   tokenId: "163000801",
   hash: "0x4a43c182a8d93c6ba286665ab5dcf77fcb4f79e6a222a78d153304eb573b2edb",
 };
