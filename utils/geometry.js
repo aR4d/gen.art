41e8aacd70ff2f975452c04a9bd8b3b807922cf6
AlexandruRad <alexandru.rad@icloud.com>
2023-04-04 22:06:04 +0300

    separate methods to draw/get points of polygon

--- utils/geometry.js
+++ utils/geometry.js
@@ -1,9 +1,19 @@
 const midpoint = ([x1, y1], [x2, y2]) => [(x1 + x2) / 2, (y1 + y2) / 2];
 
-// regular polygon
-function polygon(ctx, x, y, sides, size, rotation) {
+// gets the points for a regular polygon [[x1, y1], [x2, y2], ...]
+function polygon_points(x, y, sides, size, rotation) {
   const theta = (rotation * PI) / 180;
-  ctx.beginPath(), ctx.moveTo(x + size * cos(theta), y + size * sin(theta));
+  const pts = [];
+  for (var i = 1; i <= sides; i++)
+    pts.push([x + size * cos((i * 2 * PI) / sides + theta), y + size * sin((i * 2 * PI) / sides + theta)]);
+  return pts;
+}
+
+// draws regular polygon
+function draw_polygon(ctx, x, y, sides, size, rotation) {
+  const theta = (rotation * PI) / 180;
+  ctx.beginPath();
   for (var i = 1; i <= sides; i++)
     ctx.lineTo(x + size * cos((i * 2 * PI) / sides + theta), y + size * sin((i * 2 * PI) / sides + theta));
+  ctx.closePath();
 }
