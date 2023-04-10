afbdf9e77f8798e9eaf8663e2b70420d52ea0745
AlexandruRad <alexandru.rad@icloud.com>
2023-04-10 23:21:05 +0300

    watercolor refactor

--- effects/watercolor.js
+++ effects/watercolor.js
@@ -1,32 +1,30 @@
-function watercolor(ctx) {
-  const pts = watercolor_points();
-  // console.log(pts);
-  draw_polygon_from_points(ctx, pts);
-}
-
-function watercolor_points() {
-  const pts = [];
-  const polygon = polygon_points(140, 200, 10, 100, 0);
-  // console.log(polygon);
-
-  for (let i = 0; i < polygon.length; i++) {
-    pts.push(...split(1, 15, polygon[i], polygon[(i + 1) % polygon.length]).slice(1));
-  }
-
-  return pts;
-}
-
-function split(depth, stdev, a, b) {
-  let m,
-    pts0 = [a, b];
-  for (let d = 0; d < depth; d++) {
-    let pts1 = [pts0[0]];
-    for (let i = 0; i < pts0.length - 1; i++) {
-      m = midpoint(pts0[i], pts0[i + 1]);
-      // TODO: remove round() for debugging
-      pts1.push([round(R.gaussian(m[0], stdev)), round(R.gaussian(m[1], stdev))], pts0[i + 1]);
+// Watercolor texture for a polygon defined by points [[x1, y1], [x2, y2], ...]
+class Watercolor {
+  DEPTH = 3;
+  VARIANCE = 8;
+  constructor(polygon) {
+    this.base = [];
+    for (let i = 0; i < polygon.length; i++) {
+      this.base.push(...this.split(this.DEPTH, this.VARIANCE, polygon[i], polygon[(i + 1) % polygon.length]).slice(1));
     }
-    pts0 = pts1;
   }
-  return pts0;
+
+  draw(ctx) {
+    // console.log(this.base);
+    draw_polygon_from_points(ctx, this.base);
+  }
+
+  split(depth, stdev, a, b) {
+    let m,
+      pts0 = [a, b];
+    for (let d = 0; d < depth; d++) {
+      let pts1 = [pts0[0]];
+      for (let i = 0; i < pts0.length - 1; i++) {
+        m = midpoint(pts0[i], pts0[i + 1]);
+        pts1.push([round(R.gaussian(m[0], stdev)), round(R.gaussian(m[1], stdev))], pts0[i + 1]);
+      }
+      pts0 = pts1;
+    }
+    return pts0;
+  }
 }
