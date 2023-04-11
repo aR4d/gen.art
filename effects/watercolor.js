63aebddd1b6868bfc2b7d1e9f74e08404bc5c9ce
AlexandruRad <alexandru.rad@icloud.com>
2023-04-11 23:10:26 +0300

    smooth edges - 1

--- effects/watercolor.js
+++ effects/watercolor.js
@@ -1,24 +1,23 @@
 // Watercolor texture for a polygon defined by points [[x1, y1], [x2, y2], ...]
 class Watercolor {
-  DEPTH = 3;
-  VARIANCE = 7;
-  VARIANCE_SHRINK_FACTOR = 0.8; // Decrease variance as depth increases
+  VARIANCE_SHRINK_FACTOR = 0.9; // Decrease variance as depth increases
 
   constructor(polygon) {
-    this.base = this.deform(polygon, this.DEPTH, this.VARIANCE);
+    this.base = this.deform(polygon, 3, 7);
   }
 
   draw(ctx) {
-    console.log("Points: " + this.base.length);
-    draw_polygon_from_points(ctx, this.base);
+    ctx.fillStyle = "hsla(181, 21%, 63%, 0.05)";
+
+    for (let i = 0; i < 50; i++) {
+      const layer = this.deform(this.base, 3, 7);
+      draw_polygon_from_points(ctx, layer);
+      ctx.fill();
+    }
   }
 
-  // deform_base(ctx, depth, stdev) {
-  //   draw_polygon_from_points(ctx, this.deform(this.base, depth, stdev));
-  // }
-
   deform(polygon, depth, stdev) {
-    let pts = [];
+    const pts = [];
     for (let i = 0; i < polygon.length; i++)
       pts.push(...this.split(depth, stdev, polygon[i], polygon[(i + 1) % polygon.length]).slice(1));
     return pts;
@@ -27,9 +26,10 @@ class Watercolor {
   split(depth, stdev, a, b) {
     let m,
       std0 = stdev,
-      pts0 = [a, b];
+      pts0 = [a, b],
+      pts1;
     for (let d = 0; d < depth; d++) {
-      let pts1 = [pts0[0]];
+      pts1 = [pts0[0]];
       for (let i = 0; i < pts0.length - 1; i++) {
         m = midpoint(pts0[i], pts0[i + 1]);
         pts1.push([round(R.gaussian(m[0], std0)), round(R.gaussian(m[1], std0))], pts0[i + 1]);
