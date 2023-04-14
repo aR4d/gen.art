dbb24041e929e82e868257beee27f0b0e7ba2698
AlexandruRad <alexandru.rad@icloud.com>
2023-04-14 17:03:28 +0300

    textures draft

--- utils/textures.js
+++ utils/textures.js
@@ -1,30 +1,81 @@
-// TODO: use interface!
-function draw_texture(ctx, x, y, w, h, count, minSize, maxSize) {}
-
-// draws a bunch of circles over a rectangle area, uniform distribution.
-// [x, y] - top left corner
-// w, h - width, height
-// count - number of circles to draw
-// [minSize, maxSize] - range of size for circles
-function circles_texture(ctx, x, y, w, h, count, minSize, maxSize) {
-  ctx.beginPath();
-  for (let i = 0; i < count; i++) {
-    ctx.fillStyle = `rgba(0, 0, 0, ${R.random_dec()})`;
-    ctx.arc(R.random_int(x, w), R.random_int(y, h), R.random_int(minSize, maxSize), 0, 2 * PI);
-    // TODO: moveTo necessary!!
+class Textures {
+  // topLeft, bottomRight - [x, y]
+  constructor(ctx, count, topLeft, bottomRight) {
+    this.ctx = ctx;
+    this.topLeft = topLeft;
+    this.bottomRight = bottomRight;
+    this.width = bottomRight[0] - topLeft[0];
+    this.height = bottomRight[1] - topLeft[1];
+    this.minSize = this.width * 0.02;
+    this.maxSize = this.width * 0.1;
+    this.minOpacity = 0.01;
+    this.maxOpacity = 0.5;
+    this.count = count;
   }
-  ctx.closePath();
-  ctx.fill();
-}
 
-function poylgons_texture() {}
-
-function rect_texture(ctx, x, y, w, h, count, minSize, maxSize) {
-  ctx.beginPath();
-  for (let i = 0; i < count; i++) {
-    ctx.fillStyle = `rgba(0, 0, 0, ${R.random_num(0, 0.5)})`;
-    ctx.fillRect(R.random_int(x, w), R.random_int(y, h), R.random_int(minSize, maxSize), R.random_int(minSize, maxSize));
+  info() {
+    log("");
+    log("----Texture info----");
+    log("Rectangle width: " + this.width);
+    log("Rectangle height: " + this.height);
+    log("Count: " + this.count);
+    log("Min size: " + this.minSize);
+    log("Max size: " + this.maxSize);
+    log("Min opacity: " + this.minOpacity * 100 + "%");
+    log("Max opacity: " + this.maxOpacity * 100 + "%");
+    log("");
   }
-  ctx.closePath();
-  ctx.fill();
+
+  // [[x1, y1], [x2, y2], ...]
+  clip(polygon) {
+    this.ctx.beginPath();
+    for (let i = 0; i < polygon.length; i++) {
+      this.ctx.lineTo(polygon[i][0], polygon[i][1]);
+    }
+    this.ctx.stroke();
+    // this.ctx.clip();
+  }
+
+  rectangles() {
+    for (let i = 0; i < this.count; i++) {
+      this.ctx.fillStyle = this.transparent_black();
+      this.ctx.fillRect(this.x(), this.y(), this.size(), this.size());
+    }
+  }
+
+  circles() {
+    let x, y, r;
+    for (let i = 0; i < this.count; i++) {
+      (x = this.x()), (y = this.y()), (r = ~~(this.size() / 2));
+      this.ctx.fillStyle = this.transparent_black();
+      this.ctx.moveTo(x + r, y);
+      this.ctx.arc(x, y, r, 0, 2 * PI);
+    }
+    this.ctx.fill();
+  }
+
+  line() {
+    this.ctx.moveTo(this.x(), this.y());
+    this.ctx.strokeStyle = this.transparent_black();
+    for (let i = 0; i < this.count; i++) {
+      this.ctx.lineTo(this.x(), this.y());
+    }
+    this.ctx.stroke();
+  }
+
+  y() {
+    return R.random_int(this.topLeft[1], this.bottomRight[1]);
+  }
+
+  x() {
+    return R.random_int(this.topLeft[0], this.bottomRight[0]);
+  }
+
+  size() {
+    return R.random_int(this.minSize, this.maxSize);
+  }
+
+  transparent_black() {
+    return `rgba(0, 0, 0, ${R.random_num(this.minOpacity, this.maxOpacity)})`;
+  }
 }
